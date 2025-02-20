554cf8ed2270227ebfca00187a20bd2b3149b663
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-02-20 21:53:39 +0100

    [feat] Add languages.js

---
+++ src/assets/languages.js
@@ -0,0 +1,46 @@
+const languages = [
+  {
+    id: 1,
+    titolo: "HTML",
+    autore: "Boolean",
+    contenuto: "HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.",
+    categoria: "Coding"
+  },
+  {
+    id: 2,
+    titolo: "CSS",
+    autore: "Mozilla Developer Network",
+    contenuto: "CSS (Cascading Style Sheets) è un linguaggio di stile utilizzato per descrivere la presentazione di un documento scritto in HTML o XML. Controlla il layout, i colori, i caratteri e l'aspetto complessivo di una pagina web.",
+    categoria: "Coding"
+  },
+  {
+    id: 3,
+    titolo: "JavaScript",
+    autore: "Free Code Camp",
+    contenuto: "JavaScript è un linguaggio di programmazione dinamico utilizzato per lo sviluppo web, che consente contenuti interattivi come grafici animati, pulsanti cliccabili e altre funzionalità dinamiche sui siti web.",
+    categoria: "Coding"
+  },
+  {
+    id: 4,
+    titolo: "Node.js",
+    autore: "CodeHS",
+    contenuto: "Node.js è un ambiente di runtime JavaScript lato server che permette di utilizzare JavaScript anche sul backend, consentendo agli sviluppatori di usare lo stesso linguaggio sia sul client che sul server.",
+    categoria: "Coding"
+  },
+  {
+    id: 5,
+    titolo: "Express",
+    autore: "CodeLabs",
+    contenuto: "Express è un framework per applicazioni web minimalista e veloce per Node.js. Fornisce strumenti robusti per creare applicazioni web e API e semplifica la gestione delle rotte e delle richieste.",
+    categoria: "Coding"
+  },
+  {
+    id: 6,
+    titolo: "ReactJS",
+    autore: "Khan Academy",
+    contenuto: "ReactJS è una libreria JavaScript per costruire interfacce utente, in particolare applicazioni a pagina singola. Consente agli sviluppatori di creare componenti UI riutilizzabili e di gestire efficacemente lo stato dell'applicazione.",
+    categoria: "Coding"
+  }
+];
+
+export default languages

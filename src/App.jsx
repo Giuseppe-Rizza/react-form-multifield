ce0b5f4fbbd641d22e92fa71ae7c9aae9c9787dc
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-02-21 08:27:08 +0100

    [feat] Add final step

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import languages from './assets/languages';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//  aggiungo i campi vuoti al form
+const initialFormData = {
+  titolo: "",
+  autore: "",
+  contenuto: "",
+  categoria: "",
+};
+
+export default function App() {
+
+  const [listLanguages, setListLanguages] = useState(languages);
+  const [formData, setFormData] = useState(initialFormData);
+
+  function handleFormData(e) {
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+    setFormData((currentFormData) => ({
+      ...currentFormData,
+      [e.target.name]: value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setListLanguages((currentLanguage) => [...currentLanguage, { id: currentLanguage.length === 0 ? 1 : currentLanguage[currentLanguage.length - 1].id + 1, ...formData }]);
+    setFormData(initialFormData);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <form id='formpost' onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="titolo"
+          onChange={handleFormData}
+          value={formData.titolo}
+          placeholder='Inserisci titolo'
+        />
+
+        <input
+          type="text"
+          name="autore"
+          onChange={handleFormData}
+          value={formData.autore}
+          placeholder='Inserisci autore'
+        />
+
+        <textarea
+          name="contenuto"
+          onChange={handleFormData}
+          value={formData.contenuto}
+          placeholder='Inserisci contenuto'
+        ></textarea>
+
+        <input
+          type="text"
+          name="categoria"
+          onChange={handleFormData}
+          value={formData.categoria}
+          placeholder='Inserisci categoria'
+        />
+
+        <button>Submit</button>
+      </form>
+
     </>
   )
 }
-
-export default App
